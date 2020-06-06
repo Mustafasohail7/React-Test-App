52d13a2fbeb1ce1df72f3121e59da2ba2c18e1d4
Mustufa Sohail <mustufasohail7@gmail.com>
2020-06-07 01:51:26 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
 
 function App({name, age}) {
   let Umair = {name, age};
-return <div>Hello from Mustafa Sohail</div>
+return <div>
+          Hello from Mustafa Sohail<br/>
+          <Hello/>
+       </div>
 
 }
 

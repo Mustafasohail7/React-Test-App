be1920800dd008da382067d6554713fb1a9213fb
Mustufa Sohail <mustufasohail7@gmail.com>
2020-06-07 11:46:55 +0500

    Update Hello.js

--- src/Hello.js
+++ src/Hello.js
@@ -3,9 +3,9 @@ import './Hello.css'
 
 function Hello() {
     return (
-        
+        React.createElement('span',null,"Hello World from a different js file"),
         <img src="react.png" alt="React Logo" width="700" length="500"></img>
-    )
+)
 }
 
 export default Hello;

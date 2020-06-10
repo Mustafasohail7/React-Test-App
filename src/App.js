757c4fb6e3a8bb59b48f05e90e46ab4941a5e3ac
Mustufa Sohail <mustufasohail7@gmail.com>
2020-06-10 19:16:18 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Hello from './Hello';
 function App({name, age}) {
 return <div>
           Hello World from <b>{name}</b><br/>
-          Age : {age - 10}<br/>
-          <Hello/>
+          Age : {age - 10}<hr/>
+          <Hello title="Your Turn #2" body="Passing sample text as parameter" imageURL="https://miro.medium.com/max/3200/1*EVqCcmCPgpNKxU1wzcTHgw.png"/><hr/>
           <h2>Sample List</h2>
           <ol>
             <li>List item number {age - 25}</li>
@@ -18,5 +18,3 @@ return <div>
 }
 
 export default App;
-
-

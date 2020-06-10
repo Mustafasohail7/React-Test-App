139d726b28d11b4993399a49f7add65c42f58cdc
Mustufa Sohail <mustufasohail7@gmail.com>
2020-06-11 01:08:50 +0500

    Update App.js
    Removed unnecessary content

--- src/App.js
+++ src/App.js
@@ -7,12 +7,6 @@ return <div>
           Hello World from <b>{name}</b><br/>
           Age : {age - 10}<hr/>
           <Hello title="Your Turn #2" body="Passing sample text as parameter" imageURL="https://miro.medium.com/max/3200/1*EVqCcmCPgpNKxU1wzcTHgw.png"/><hr/>
-          <h2>Sample List</h2>
-          <ol>
-            <li>List item number {age - 25}</li>
-            <li>List item number {age - 24}</li>
-            <li>List item number {age - 23}</li>
-          </ol>
        </div>
 
 }
